a844fa869e6b00041b5204f23dca496b0f8a19ca
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-17 15:51:25 +0100

    Modificação do program

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace GuessTheNumber
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameModel model = new GameModel();
+            GameView view = new GameView();
+            GameController controller = new GameController(model, view);
+
+            controller.Run();
+        }
+    }
+}
